cbdbfd232ed1b74e7b24bccd23f624a0d34c7535
Jannis Piekarek <einsjannis@gmail.com>
2025-09-25 17:50:26 +0200

    Minimal terminal with minimal shell

---
+++ script/shell.js
@@ -0,0 +1,62 @@
+export class Shell {
+  constructor(terminal) {
+    this.shouldExit = false;
+    this.terminal = terminal;
+    this.path = "/home/einsjannis/";
+    this.user = "guest";
+  }
+
+  async start() {
+    while (!this.shouldExit) {
+      this.terminal.print(this.user + "@website:" + this.path + "> ");
+      const input = await this.terminal.readLine();
+      this.parseCommand(input);
+    }
+  }
+
+  parseCommand(input) {
+    const [command, ...args] = input.split(" ");
+    switch (command) {
+      case "cd":
+        this.cd(args[0]);
+        break;
+      case "exit":
+        this.exit();
+        break;
+      default:
+        this.terminal.print("Unknown command");
+    }
+  }
+
+  cd(path) {
+    function is_absolute(path) {
+      return path.startsWith("/");
+    }
+
+    function simplify(path) {
+      const parts = path.split("/");
+      const stack = [];
+
+      for (const part of parts) {
+        if (part === "..") {
+          stack.pop();
+        } else if (part !== "." && part !== "") {
+          stack.push(part);
+        }
+      }
+
+      return "/" + stack.join("/");
+    }
+
+    if (is_absolute(path)) {
+      this.path = path;
+    } else {
+      this.path = this.path + "/" + path;
+      this.path = simplify(this.path);
+    }
+  }
+
+  exit() {
+    this.shouldExit = true;
+  }
+}
---
+++ script/terminal.js
@@ -0,0 +1,48 @@
+import { TODO } from "./util.js";
+
+export class Terminal {
+  constructor(element) {
+    this.element = element;
+    this.element.classList.add("terminal");
+    this.newLine();
+  }
+
+  currentLine() {
+    return this.element.lastChild;
+  }
+
+  newLine() {
+    const line = document.createElement("p");
+    this.element.appendChild(line);
+  }
+
+  print(message) {
+    this.currentLine().textContent += message;
+  }
+
+  println(message) {
+    this.currentLine().textContent += message;
+    this.newLine();
+  }
+
+  readLine() {
+    return new Promise((resolve) => {
+      let input = this.currentLine().appendChild(
+        document.createElement("input"),
+      );
+      input.focus();
+      new Promise((resolve) => {
+        input.addEventListener("keydown", (event) => {
+          if (event.key === "Enter") {
+            event.preventDefault();
+            input.readOnly = true;
+            resolve(input.value);
+          }
+        });
+      }).then((inputText) => {
+        this.newLine();
+        resolve(inputText);
+      });
+    });
+  }
+}
